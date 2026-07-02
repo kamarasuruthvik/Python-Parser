efa38eb317df86f76f11bdf91739ba0c3209be04
Ruthvik Kamarasu <ruthvikkamarasu@Ruthviks-MacBook-Air.local>
2026-07-01 20:26:46 -0700

    feat: parser logic improved with streaming and single pass

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from utility import Utility
 
 class CSVHandler:
     def __init__(self):
-        self.logs=  []
         self.user_entries = dict()
         self.log_level_entries = dict()
         self.utility = Utility()
@@ -26,36 +25,26 @@ class CSVHandler:
                 
                 # Perform Aggregation
 
-                self.logs.append(row_dict)
+                user_name = row_dict["user"]
+                log_level = row_dict["level"]
+                self.utility.increment_count_key(user_name, self.user_entries)
+                self.utility.increment_count_key(log_level, self.log_level_entries)
+                [self.users_with_errors.append(user_name) if log_level == "ERROR" else None]
             
 
 
-        print(self.logs)
-
-
-
-
-    def count_entries(self):
-        for log in self.logs:
-            user_name = log['user']
-            log_level = log['level']
-            self.utility.increment_count_key(user_name, self.user_entries)
-            self.utility.increment_count_key(log_level, self.log_level_entries)
-            [self.users_with_errors.append(user_name) if log_level == "ERROR" else None]
-        
-
-        print(self.user_entries)
-        print(self.log_level_entries)
         print(self.users_with_errors)
+        print(self.log_level_entries)
+        print(self.user_entries)
+
+
 
 
     def clear_logs(self):
-        self.logs.clear()
         self.log_level_entries.clear()
         self.user_entries.clear()
         self.users_with_errors.clear()
 
 csv_handler = CSVHandler()
 csv_handler.parse_logs('data.csv')
-csv_handler.count_entries()
 csv_handler.clear_logs()
