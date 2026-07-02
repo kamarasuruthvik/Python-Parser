0f577a5ba9fced06376a560cd581a2310d4d0f56
Ruthvik Kamarasu <ruthvikkamarasu@Ruthviks-MacBook-Air.local>
2026-07-01 22:10:31 -0700

    feat: added capabilities to detect error spikes

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ class CSVHandler:
         self.user_entries = dict()
         self.log_level_entries = dict()
         self.utility = Utility()
-        self.users_with_errors = []
-
+        self.users_with_errors = dict()
+        self.error_minutes = dict()
 
 
     def parse_logs(self, filename):
@@ -25,18 +25,40 @@ class CSVHandler:
                 
                 # Perform Aggregation
 
-                user_name = row_dict["user"]
-                log_level = row_dict["level"]
-                self.utility.increment_count_key(user_name, self.user_entries)
-                self.utility.increment_count_key(log_level, self.log_level_entries)
-                [self.users_with_errors.append(user_name) if log_level == "ERROR" else None]
-            
+                user_name = row_dict.get("user")
+                log_level = row_dict.get("level")
+                timestamp = row_dict.get("time")
+                timestamp_minute_rounded = self.utility.convert_hh_mm_ss_to_hh_mm(timestamp)
+
+
+
+                self.utility.increment_count_key(user_name, self.user_entries) # user entries
+                self.utility.increment_count_key(log_level, self.log_level_entries) # log level entries
+                if log_level == "ERROR":
+                    if user_name is not None:
+                        self.utility.increment_count_key(user_name, self.users_with_errors)
+                    self.utility.increment_count_key(timestamp_minute_rounded, self.error_minutes) # minutes with errors
+                
+
+
 
 
         print(self.users_with_errors)
         print(self.log_level_entries)
         print(self.user_entries)
+        print(self.error_minutes)
 
+
+    def detect_spikes(self):
+        spikes = []
+        for minute, errors in self.error_minutes.items():
+            if errors >= 2:
+                spikes.append(minute)
+
+        return spikes
+
+    def detect_top_two_errored_users(self):
+        return sorted(self.users_with_errors.items(), key= lambda x: x[1], reverse=True)[:2]
 
 
 
@@ -44,7 +66,10 @@ class CSVHandler:
         self.log_level_entries.clear()
         self.user_entries.clear()
         self.users_with_errors.clear()
+        self.error_minutes.clear()
 
 csv_handler = CSVHandler()
 csv_handler.parse_logs('data.csv')
+print(csv_handler.detect_spikes())
+print(csv_handler.detect_top_two_errored_users())
 csv_handler.clear_logs()
--- utility.py
+++ utility.py
@@ -1,3 +1,5 @@
+import datetime
+
 class Utility:
     def increment_count_key(self, key, data):
         if key is None:
@@ -6,4 +8,11 @@ class Utility:
             data[key] = 1
         else:
             data[key] += 1
+
+    def convert_hh_mm_ss_to_hh_mm(self, timestamp):
+        timestamp_arr = timestamp.split(":")
+        hour = timestamp_arr[0]
+        minute = timestamp_arr[1]
+        return ":".join([hour, minute])
+        
 
